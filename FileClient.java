1fdd408b2017538c9934e65c7c6f16524eb4433b
KOTTEESWARAN2008 <ramusvc957@gmail.com>
2026-09-08 15:52:51 +0530

    Update FileClient.java

--- FileClient.java
+++ FileClient.java
@@ -4,36 +4,54 @@ import java.util.Scanner;
 
 public class FileClient {
     public static void main(String[] args) {
-        String serverIP = "192.168.17.183"; // Put your computer's IP here
+
+        String serverIP = "192.168.17.184";
         int port = 5000;
 
         try (Socket socket = new Socket(serverIP, port)) {
+
             Scanner scanner = new Scanner(System.in);
+
             System.out.print("Enter file name to request: ");
             String fileName = scanner.nextLine();
 
             // Send file name to server
-            PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+            PrintWriter writer =
+                new PrintWriter(socket.getOutputStream(), true);
+
             writer.println(fileName);
 
             // Read response from server
-            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            BufferedReader reader =
+                new BufferedReader(
+                    new InputStreamReader(socket.getInputStream()));
+
             String response = reader.readLine();
 
             if ("EXISTS".equals(response)) {
+
                 InputStream in = socket.getInputStream();
-                FileOutputStream fileOutput = new FileOutputStream("received_" + fileName);
-                
+
+                FileOutputStream fileOutput =
+                    new FileOutputStream("received_" + fileName);
+
                 byte[] buffer = new byte[4096];
                 int bytesRead;
+
                 while ((bytesRead = in.read(buffer)) != -1) {
                     fileOutput.write(buffer, 0, bytesRead);
                 }
+
                 fileOutput.close();
-                System.out.println("File downloaded successfully as: received_" + fileName);
+
+                System.out.println(
+                    "File downloaded successfully as: received_" + fileName);
+
             } else {
-                System.out.println("Error: File does not exist on the server.");
+                System.out.println(
+                    "Error: File does not exist on the server.");
             }
+
         } catch (IOException ex) {
             ex.printStackTrace();
         }
